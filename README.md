5764058add49c44546dc8a484b09c970470f5265
Fireatoms <34360113+Fireatoms@users.noreply.github.com>
2018-04-17 22:37:08 +0800

    fix #5 fix #6 flask config (#14)

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * [LOU57039504](https://github.com/githublost/jobplus5-16)
 * [emiyasyrou](https://github.com/yangxianxu/jobplus5-16)
 * [LOU745860922](https://github.com/834542871/jobplus5-16)
+* [Fire_atoms](https://github.com/Fireatoms/jobplus5-16)
---
+++ jobplus/config.py
@@ -0,0 +1,19 @@
+class BaseConfig(object):
+	SECRET_KEY = 'louplus 5-16'
+	INDEX_PER_PAGE = 15
+
+class DevelopmentConfig(BaseConfig):
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/simpledu?charset=utf8'
+
+class ProductionConfig(DevelopmentConfig):
+	DEBUG = False
+
+class TestingConfig(BaseConfig):
+	pass
+
+configs = {
+	'development': DevelopmentConfig,
+	'production': ProductionConfig,
+	'testing': TestingConfig
+}
